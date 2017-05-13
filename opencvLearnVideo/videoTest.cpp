0100228c585933e5f5a7bb5ceb6a92f91adc0b18
lgdlkq <1732109940@qq.com>
2017-05-13 17:37:55 +0800

    添加从摄像头采集图像

--- opencvLearnVideo/videoTest.cpp
+++ opencvLearnVideo/videoTest.cpp
@@ -2,7 +2,7 @@
 using namespace cv;
 
 int main() {
-
+	/*
 	//读取并播放视频
 	VideoCapture capture("m.avi");//读取视频
 	while (1) {//循环显示每一帧视频内容
@@ -11,6 +11,14 @@ int main() {
 		imshow("读取视频",frame);//显示当前帧
 		waitKey(30);//延时30ms
 	}
-
+	*/
+	//调用摄像头采集图像
+	VideoCapture capture(0);//从摄像头读入视频
+	while (1) {
+		Mat frame;
+		capture >> frame;//读取当前帧
+		imshow("读取摄像头", frame);//显示当前帧
+		waitKey(30);//延时30ms
+	}
 	return 0;
 }
